cece1e9b0ffd286c9fd6b22920eb566dc6a105d1
Jade <jywfang@gmail.com>
2018-02-06 14:04:50 +0100

    added background and one page styling

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -8,4 +8,14 @@ describe 'Index Page', type: :feature do
       expect(page).to have_content 'FizzBuzz'
     end
   end
+
+  it 'renders footer partial' do
+  visit '/'
+
+  expect(page).to have_selector 'footer'
+    within 'footer' do
+      expect(page).to have_content 'My Portfolio'
+      expect(page).to have_content 'Built using the awesome Middleman framework'
+    end
+  end
 end
