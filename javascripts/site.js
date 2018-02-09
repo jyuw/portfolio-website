ffc6ca04d295e6c5ff5ee3dd9499127ec66ba1d9
Jade <jywfang@gmail.com>
2018-02-09 14:36:28 +0100

    Automated commit at 2018-02-09 13:36:27 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//   alert("Hello");
+// });
+//
+// $(window).scroll(function() {
+//   var bottom_of_window = $(window).scrollTop() + 200;
+//   $('.hideme').each(function(i) {
+//     var bottom_of_object = $(this).offset().top;
+//     if (bottom_of_window > bottom_of_object) {
+//       $(this).stop().animate({'opacity': '1'}, 500);
+//     } else {
+//       $(this).stop().animate({'opacity': '0'}, 500);
+//     }
+//   });
+// });
